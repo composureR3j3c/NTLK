5834048925d0ff54df5198a941fe3738b9aaf442
bereket <gezahegnebereket@gmail.com>
2023-05-18 12:31:08 +0300

    google

---
+++ 5_machineTranslation.py
@@ -0,0 +1,28 @@
+import nltk
+
+nltk.download('comtrans')
+
+from nltk.corpus import comtrans
+
+words = comtrans.words("alignment-en-fr.txt")
+
+for word in words[:20]:
+    print(word)
+print("...")
+
+
+from nltk.translate import Alignment, AlignedSent
+
+als = AlignedSent( ["Reprise", "de", "la", "session" ], \
+    ["Resumption", "of", "the", "session" ] , \
+    Alignment( [ (0 , 0), (1 , 1), (2 , 2), (3 , 3) ] ) )
+
+from nltk.translate import IBMModel1
+
+com_ibm1 = IBMModel1(comtrans.aligned_sents()[:10], 100)
+
+print(round(com_ibm1.translation_table["bitte"]["Please"], 3) )
+
+print(round(com_ibm1.translation_table["Sitzungsperiode"]["session"] , 3) )
+
+print(round(com_ibm1.translation_table["ሰው"]["human"] , 3) )
